f055350d2dcef9ab76f7d2da9a3298d73742175c
Aamir Alam <aamiralam1991@gmail.com>
2021-03-26 00:09:43 +0530

    Add post create, read and delete routes for users

--- routes/posts/v1/post.js
+++ routes/posts/v1/post.js
@@ -9,7 +9,7 @@ const User = require("../../../models/User");
 //middleware
 const auth = require("../../../middleware/auth");
 
-// @route    POST api/posts
+// @route    POST api/posts/v1
 // @desc     Create a post
 // @access   Private
 router.post(
@@ -41,5 +41,63 @@ router.post(
     }
   }
 );
+
+// @route    GET api/posts/v1
+// @desc     Get all posts
+// @access   Private
+router.get("/", auth, async (req, res) => {
+  try {
+    const posts = await Post.find().limit(20).sort({ date: -1 });
+    res.json(posts);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
+// @route    GET api/posts/v1/:id
+// @desc     Get post by ID
+// @access   Private
+router.get("/:id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (!post) {
+      return res.status(404).json({ msg: "Post not found" });
+    }
+
+    res.json(post);
+  } catch (err) {
+    console.error(err.message);
+
+    res.status(500).send("Server Error");
+  }
+});
+
+// @route    DELETE api/posts/v1/:id
+// @desc     Delete a post
+// @access   Private
+router.delete("/:id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (!post) {
+      return res.status(404).json({ msg: "Post not found" });
+    }
+
+    // Check user
+    if (post.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: "User not authorized" });
+    }
+
+    await post.remove();
+
+    res.json({ msg: "Post removed" });
+  } catch (err) {
+    console.error(err.message);
+
+    res.status(500).send("Server Error");
+  }
+});
 
 module.exports = router;
